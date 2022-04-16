34529744e8ffe175a0332720be0d2248ffcf9446
Bdiebeak <123redcreeper123@gmail.com>
2022-04-16 23:40:00 +0300

    Setting up to Level transition from MainMenu

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,26 @@
+﻿using RollingBall.Game.SceneLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RollingBall.UI
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private Button startButton;
+
+        private void OnEnable()
+        {
+            startButton.onClick.AddListener(StartButtonClickHandler);
+        }
+        
+        private void OnDisable()
+        {
+            startButton.onClick.RemoveListener(StartButtonClickHandler);
+        }
+
+        private void StartButtonClickHandler()
+        {
+            ScenesLoader.Load(Scenes.Level);
+        }
+    }
+}

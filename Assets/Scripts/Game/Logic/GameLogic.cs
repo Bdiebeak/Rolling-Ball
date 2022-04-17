213574fdc8311ea1e82ec082988ad8248f09420c
Bdiebeak <123redcreeper123@gmail.com>
2022-04-18 01:41:12 +0300

    Adding GameLogic scipt

---
+++ Assets/Scripts/Game/Logic/GameLogic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using RollingBall.Game.Score;
+using RollingBall.Helpers;
+using RollingBall.UI;
+using UnityEngine;
+
+namespace RollingBall.Game.Logic
+{
+    public class GameLogic : MonoBehaviour
+    {
+        [SerializeField] private TriggerHandler startHandler;
+        [SerializeField] private TriggerHandler finishHandler;
+        [SerializeField] private TriggerHandler gameOverHandler;
+        [Space]
+        [SerializeField] private CurrentScorePanel currentScorePanel;
+        [SerializeField] private FinishPanel finishPanel;
+        [SerializeField] private CanvasGroupPanel gameOverPanel;
+        [Space]
+        [SerializeField] private ScoreCounter scoreCounter;
+        
+        private void OnEnable() => SubscribeOnRequiredEvents();
+        private void SubscribeOnRequiredEvents()
+        {
+            startHandler.entered += OnStartHandler;
+            finishHandler.entered += OnFinishHandler;
+            gameOverHandler.entered += OnGameOverHandler;
+        }
+
+        private void OnDisable() => UnsubscribeOnRequiredEvents();
+        private void UnsubscribeOnRequiredEvents()
+        {
+            startHandler.entered -= OnStartHandler;
+            finishHandler.entered -= OnFinishHandler;
+            gameOverHandler.entered -= OnGameOverHandler;
+        }
+        
+        private void OnStartHandler(GameObject enteredObject)
+        {
+            scoreCounter.StartCount();
+
+            currentScorePanel.scoreCounter = scoreCounter;
+            currentScorePanel.ActivateCanvasGroup();
+            finishPanel.DeactivateCanvasGroup();
+            gameOverPanel.DeactivateCanvasGroup();
+        }
+        
+        private void OnFinishHandler(GameObject enteredObject)
+        {
+            scoreCounter.StopCount();
+            gameOverHandler.gameObject.SetActive(false);
+            
+            finishPanel.ChangeScoreText(scoreCounter.CurrentScore);
+            finishPanel.ActivateCanvasGroup();
+            currentScorePanel.DeactivateCanvasGroup();
+            gameOverPanel.DeactivateCanvasGroup();
+            
+            UnitOfWork.Instance.ScoreRepository.Add(new ScoreData(scoreCounter.CurrentScore, DateTime.Now));
+        }
+        
+        private void OnGameOverHandler(GameObject enteredObject)
+        {
+            scoreCounter.StopCount();
+
+            gameOverPanel.ActivateCanvasGroup();
+            currentScorePanel.DeactivateCanvasGroup();
+            finishPanel.DeactivateCanvasGroup();
+        }
+    }
+}
